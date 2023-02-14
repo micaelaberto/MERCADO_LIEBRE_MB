e4c59c2f9491d9a102f5a0ab542fb759fdb2a3d2
Micaela Berto <micaela@MBP-de-Micaela.fibertel.com.ar>
2023-02-14 15:15:03 -0300

    index

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/home.html'))
 })
 
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.post('/registro', (req, res) => {
+    console.log(req.body);
+    users.push(req.body);
+    res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
+});
+
+app.post('/login', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
 const port = process.env.PORT || 3001;
 app.listen(port, () => 
     console.log(`Servere started on http://localhost:${port}`)
